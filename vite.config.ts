51f7535baa70dd4cef7b77e8dfdeea930d03d42c
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2025-10-16 15:43:44 +0200

    small error correction, import node:fs and node:path

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig(({ mode }) => ({
 			formats: ['es'],
 			fileName: 'index'
 		},
+		rollupOptions: {
+			external: [
+				'node:fs',
+				'node:path'
+			]
+		}
 	}
 }));
